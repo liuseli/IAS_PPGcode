9a2a5f05b8cef55cd07fa8f50e34dc1a3f56657d
Elias Lius <elias.lius@aalto.fi>
2020-04-25 13:11:22 +0300

    Added craphs and STD calculation. not working yet tho

--- ppg_ias/lib/home.dart
+++ ppg_ias/lib/home.dart
@@ -1,7 +1,8 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:wakelock/wakelock.dart';
-/*import 'chart.dart';*/
+import 'dart:math';
+import 'waveform.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,13 +15,21 @@ class HomePageView extends State<HomePage> {
   bool _toggled = false;
   bool _processing = false;
   List<SensorValue> _data = [];
+  List<SensorStats> _statdata = [];
   CameraController _controller;
+  double _stdMean = 0;
+  double _alpha = 0.3;
+  int _bpm = 0;
+  int _k = pow(2,2);
+
     _toggle() {
       _initController().then((onValue) {
       setState(() {
         _toggled = true;
         _processing = false;
       });
+      _updateBPM();
+      _updateStdmean();
     });
   }
 
@@ -58,21 +67,91 @@ class HomePageView extends State<HomePage> {
       _controller = null;
   }
 
+   _updateBPM() async {
+    List<SensorValue> _values;
+    double _avg;
+    int _n;
+    double _m;
+    double _threshold;
+    double _bpm;
+    int _counter;
+    int _previous;
+    while (_toggled) {
+      _values = List.from(_data);
+      _avg = 0;
+      _n = _values.length;
+      _m = 0;
+      _values.forEach((SensorValue value) {
+        _avg += value.value / _n;
+        if (value.value > _m) _m = value.value;
+      });
+      _threshold = (_m + _avg) / 2;
+      _bpm = 0;
+      _counter = 0;
+      _previous = 0;
+      for (int i = 1; i < _n; i++) {
+        if (_values[i - 1].value < _threshold &&
+            _values[i].value > _threshold) {
+          if (_previous != 0) {
+            _counter++;
+            _bpm +=
+                60000 / (_values[i].time.millisecondsSinceEpoch - _previous);
+          }
+          _previous = _values[i].time.millisecondsSinceEpoch;
+        }
+      }
+      if (_counter > 0) {
+        _bpm = _bpm / _counter;
+        setState(() {
+          _bpm = (1 - _alpha) * _bpm + _alpha * _bpm;
+        });
+      }
+      await Future.delayed(Duration(milliseconds: (1000 * 50 / 30).round()));
+    }
+  }
+
+
   _scanImage(CameraImage image) {
+    int _n = image.planes.first.bytes.length;
     double _avg =
-        image.planes.first.bytes.reduce((value, element) => value + element) /
-            image.planes.first.bytes.length;
+        image.planes.first.bytes.reduce((value, element) => value + element) / _n;
+    double _std = 0;
+    image.planes.first.bytes.forEach((value) => 
+        ((_std + pow((value - _avg),2))/_n));
+    _std = sqrt(_std);
+
     if (_data.length >= 50) {
       _data.removeAt(0);
     }
     setState(() {
       _data.add(SensorValue(DateTime.now(), _avg));
+      _statdata.add(SensorStats(DateTime.now(), _std));
     });
     Future.delayed(Duration(milliseconds: 1000 ~/ 30)).then((onValue) {
       setState(() {
         _processing = false;
       });
     });
+  }
+
+  _updateStdmean() async {
+    List<SensorStats> _devs;
+    int _n;
+    double _m;
+    while (_toggled) {
+      _devs = List.from(_statdata);
+      _n = _devs.length;
+      _m = 0;
+      _stdMean = 0;
+      _devs.forEach((SensorStats std) {
+        _stdMean += std.std / _n;
+        if (std.std > _m) _m = std.std;
+      });
+      setState(() {
+        _stdMean = _stdMean;
+      });
+      await Future.delayed(Duration(milliseconds: (1000 * 50 / 30).round()));
+    }
   }
 
   @override
@@ -89,50 +168,70 @@ class HomePageView extends State<HomePage> {
         child: Column(
           children: <Widget>[
             Expanded(
-              child: Row(
+              child: Column(
                 children: <Widget>[
                   Expanded(
-                    child: Container(
-                      color: Colors.red,
-                      child: Center(
-                        child: Text('HRV'),
-                      ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            color: Colors.red,
+                            child: Center(
+                              child: Text(List.from(_statdata) == null ? _k.toString() : _stdMean.round().toString()),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.red,
+                            child: Center(
+                              child: Text((_bpm < 0 ? _bpm.round().toString() : "--"),
+                              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   Expanded(
-                    child: Container(
-                      color: Colors.red,
-                      child: Center(
-                        child: Text('BPM')
-                      ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            color: Colors.white,
+                            child: Center(
+                              child: Text('Resp rate')
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.white,
+                            child: Center(
+                              child: Text('Emotion')
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
-              
               ),
             ),
+
             Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      color: Colors.white,
-                      child: Center(
-                        child: Text('Resp rate')
-                      ),
+              child: Container(
+                margin: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(18),
                     ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.white,
-                      child: Center(
-                        child: Text('Emotion')
-                      ),
-                    ),
-                  ),
-                ]
-              )
+                    color: Colors.black),
+                child: Chart(_data),
+              ),
             ),
+
             Expanded(
               child: Row(
                 children: <Widget>[
@@ -167,10 +266,4 @@ class HomePageView extends State<HomePage> {
       ),
     );
   }
-}
-
-class SensorValue {
-  final DateTime time;
-  final double value;
-  SensorValue(this.time, this.value);
 }
